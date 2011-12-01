10fb139686525b395b94f928251091e40fb662e9
Erik Salter <esalter@bnivideo.com>
2011-12-01 10:05:52 -0500

    Updated 5.1.0 with BNI-specific build parameters.

--- query/src/main/java/org/infinispan/query/impl/EntityLoader.java
+++ query/src/main/java/org/infinispan/query/impl/EntityLoader.java
@@ -1,24 +1,18 @@
 /*
- * JBoss, Home of Professional Open Source
- * Copyright 2011 Red Hat Inc. and/or its affiliates and other
- * contributors as indicated by the @author tags. All rights reserved.
- * See the copyright.txt in the distribution for a full listing of
- * individual contributors.
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * JBoss, Home of Professional Open Source Copyright 2011 Red Hat Inc. and/or
+ * its affiliates and other contributors as indicated by the @author tags. All
+ * rights reserved. See the copyright.txt in the distribution for a full listing
+ * of individual contributors. This is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the License,
+ * or (at your option) any later version. This software is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU Lesser General Public License for more details. You should have
+ * received a copy of the GNU Lesser General Public License along with this
+ * software; if not, write to the Free Software Foundation, Inc., 51 Franklin
+ * St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF site:
+ * http://www.fsf.org.
  */
 
 package org.infinispan.query.impl;
@@ -37,27 +31,41 @@ import org.infinispan.query.backend.KeyTransformationHandler;
  * @since 5.0
  */
 public class EntityLoader {
-   
-   private final AdvancedCache<?, ?> cache;
-   private final KeyTransformationHandler keyTransformationHandler;
-   
-   public EntityLoader(Cache<?, ?> cache, KeyTransformationHandler keyTransformationHandler) {
-      this.keyTransformationHandler = keyTransformationHandler;
-      this.cache = cache.getAdvancedCache();
-   }
 
-   public Object load(EntityInfo entityInfo) {
-      Object cacheKey = keyTransformationHandler.stringToKey(entityInfo.getId().toString(), cache.getClassLoader());
-      return cache.get(cacheKey);
-   }
+    private final AdvancedCache<?, ?> cache;
+    private final KeyTransformationHandler keyTransformationHandler;
 
-   public List<Object> load(EntityInfo... entityInfos) {
-      int size = entityInfos.length;
-      ArrayList<Object> list = new ArrayList<Object>(size);
-      for (EntityInfo e : entityInfos) {
-         list.add(load(e));
-      }
-      return list;
-   }
+    public EntityLoader(Cache<?, ?> cache, KeyTransformationHandler keyTransformationHandler) {
+        this.keyTransformationHandler = keyTransformationHandler;
+        this.cache = cache.getAdvancedCache();
+    }
 
+    public Object load(EntityInfo entityInfo) {
+        Object cacheKey = keyTransformationHandler.stringToKey(entityInfo.getId().toString(), cache.getClassLoader());
+        return cache.get(cacheKey);
+    }
+
+    public Object[] loadWithScores(EntityInfo entityInfo) {
+        Object cacheKey = keyTransformationHandler.stringToKey(entityInfo.getId().toString(), cache.getClassLoader());
+        Float score = (Float) entityInfo.getProjection()[0];
+        return new Object[] { cache.get(cacheKey), score };
+    }
+
+    public List<Object> load(EntityInfo... entityInfos) {
+        int size = entityInfos.length;
+        ArrayList<Object> list = new ArrayList<Object>(size);
+        for (EntityInfo e : entityInfos) {
+            list.add(load(e));
+        }
+        return list;
+    }
+
+    public List<Object[]> loadWithScores(EntityInfo... entityInfos) {
+        int size = entityInfos.length;
+        ArrayList<Object[]> list = new ArrayList<Object[]>(size);
+        for (EntityInfo e : entityInfos) {
+            list.add(loadWithScores(e));
+        }
+        return list;
+    }
 }
